dd5cdf4848cadf09f001c1aabaf1edd2840aa79e
Anders Christensen <89982133+McAndersC@users.noreply.github.com>
2022-05-11 13:50:27 +0200

    fixed issued

--- client/js/templates.js
+++ client/js/templates.js
@@ -6,8 +6,9 @@ templates.template01 = (moment, template) => {
     <section class="template1">
         <img src="/moments/${moment.id}/assets/${moment.id}/${template.media[0].image}" class="temp1-pic1" alt="">
         <div class="text-pic1">${template.media[0].text}</div>
+        ${template.media.length === 1 ? '' : `
         <img src="/moments/${moment.id}/assets/${moment.id}/${template.media[1].image}" class="temp1-pic2" alt="">
-        <div class="text-pic2">${template.media[1].text}</div>
+        <div class="text-pic2">${template.media[1].text}</div>`}
     </section>
     `
 }
